ca441275eaef424ca960f2fd3db92a307acb7d0a
valneibritosoares <vbs2003@gmail.com>
2022-08-02 19:30:15 -0300

    Inicio do código Urna

---
+++ etapas.js
@@ -0,0 +1,60 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 25444,
+                nome: 'Fulano de Tal',
+                partido: 'PTSW',
+                fotos:[
+                    {url:'25444.jpg', legenda: 'Vereador', }
+                ]
+            },
+            {
+                numero: 88333,
+                nome: 'Beltrano da Silva',
+                partido: 'PXS',
+                fotos:[
+                    {url:'88333.jpg', legenda: 'Vereador', }
+                ]
+            },
+            ]
+        },
+        {
+            titulo: 'PREFEITO',
+            numeros: 2,
+            candidatos: [
+                {
+                    numero: 13,
+                    nome: 'Ciclano',
+                    partido: 'PTL',
+                    vice: 'Cic',
+                    fotos: [
+                        {url:'13.png', legenda: 'Prefeito'},
+                        {url: '13-2.png', legenda: 'Vice-Prefeito', small: true}
+                    ]
+                },
+                {
+                    numero: 26,
+                    nome: 'Zulano',
+                    partido: 'PVG',
+                    vice: 'Zé',
+                    fotos: [
+                        {url:'26.png', legenda: 'Prefeito'},
+                        {url: '26-2.png', legenda: 'Vice-Prefeito', small: true}
+                    ]
+                },
+                {
+                    numero: 78,
+                    nome: 'Francis',
+                    partido: 'PMAJ',
+                    vice: 'Berne',
+                    fotos: [
+                        {url:'78.jpg', legenda: 'Prefeito'},
+                        {url: '78-2.jpg', legenda: 'Vice-Prefeito', small: true}
+                    ]
+                },
+            ]
+        }
+    ];
